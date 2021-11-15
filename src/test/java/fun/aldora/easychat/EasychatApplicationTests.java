8e597d518b355fdaac806106455b483aef530d50
ShakalakaB <yunjie989@gmail.com>
2021-11-15 17:08:25 +0800

    Build dependencies

---
+++ src/test/java/fun/aldora/easychat/EasychatApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.aldora.easychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasychatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
